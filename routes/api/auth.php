865255af64d81271be9722a4d70dbf3cf358d09b
JJ👨‍💻 <112809736+jericjohnnn@users.noreply.github.com>
2025-02-13 00:44:34 +0800

    ge laay mode otp

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -7,3 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'userLogin']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
